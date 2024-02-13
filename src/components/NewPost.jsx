db350a127da894b01c709a32940d99f5015b680c
Harsh Varshney <harsh141994@gmail.com>
2024-02-13 12:10:43 -0800

    Lifting State

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,22 +1,17 @@
 import classes from "./NewPost.module.css";
 import { useState } from "react";
 
-function NewPost() {
-  const [enteredBody, setEnteredBody] = useState("Initial state");
-
-  function changeBodyHandler(event) {
-    setEnteredBody(event.target.value);
-  }
+function NewPost(props) {   // props is something we automatically get (this is passed from the parent)
+ 
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
